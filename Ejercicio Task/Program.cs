15ff59a75e8f623f486ca687b24a54d988e3772d
charly1102 <cmombru@yahoo.com>
2024-04-22 21:07:30 -0300

    Crear Proyecto

---
+++ Ejercicio Task/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] nums = { 5, 2, 9, 1, 3 };
+
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        if (nums[j] > nums[j + 1])
+        {
+            int aux = nums[j];
+            nums[j] = nums[j + 1];
+            nums[j + 1] = aux;
+        }
+    }
+}
+
+for ( int i = 0; i < 7; i++)
+{
+    Console.WriteLine(nums[i]);
+}
